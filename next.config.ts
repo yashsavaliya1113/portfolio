d2563cf574f16fbfddc618bec209a36d20cc8795
Yash Savaliya <tpyashsavaliya13092002@gmail.com>
2026-07-05 20:12:47 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,16 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
   trailingSlash: true,
+
+  basePath: isProd ? "/portfolio" : "",
+  assetPrefix: isProd ? "/portfolio/" : "",
 };
 
 export default nextConfig;
